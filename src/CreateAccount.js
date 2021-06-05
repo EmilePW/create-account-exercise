530dbf499465d7f9fbf229b1f62d7a7b9340f653
Emile Paffard-Wray <emilepw@Emiles-MacBook-Air.local>
2021-06-05 16:23:18 +0100

    Added solution

--- src/CreateAccount.js
+++ src/CreateAccount.js
@@ -15,10 +15,54 @@
     the response in a state variable).
 */
 
-import React from "react";
+import React, { useState } from "react";
 
 const CreateAccount = () => {
-  return <div className="CreateAccount"></div>;
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [userResponse, setUserResponse] = useState(null);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    fetch("https://cyf-cors-api.herokuapp.com/user", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ name, email, password })
+    })
+      .then(response => response.json())
+      .then(data => {
+        const stringifiedData = JSON.stringify(data, null, 2);
+        setUserResponse(stringifiedData);
+      })
+  }
+
+  return (
+    <div className="CreateAccount">
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={name}
+          onChange={(event) => setName(event.target.value)}
+        />
+        <input
+          type="email"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+        />
+        <input
+          type="password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        />
+        <button type="submit">Submit</button>
+      </form>
+      <p>{userResponse}</p>
+    </div>
+  );
 };
 
 export default CreateAccount;
